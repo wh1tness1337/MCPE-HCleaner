3f973705ef4735e4babf3b2f11331626d60d836d
meLdozy <149616933+meLdozyk@users.noreply.github.com>
2024-01-14 18:29:16 +0300

    Add files via upload

---
+++ Tool1.bat
@@ -0,0 +1,54 @@
+@ECHO OFF
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Successfully
+) else (
+    powershell -Command "Start-Process '%0' -Verb RunAs"
+    exit
+)
+
+COLOR A
+CLS
+FOR /F "tokens=*" %%G IN ('wevtutil.exe el') DO (call :do_clear "%%G")
+:ECHO Successfully
+REG DELETE "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache" /va /f
+REG DELETE "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /f
+REG DELETE "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\BagMRU" /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Bags" /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /va /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\FirstFolder" /va /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU" /va /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRULegacy" /va /f
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU" /f
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU"
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache" /va /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\AppCompatCache" /va /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RADAR\HeapLeakDetection\DiagnosedApplications" /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RADAR\HeapLeakDetection\DiagnosedApplications"
+FOR /F "tokens=2" %%i IN ('whoami /user /fo table /nh') DO SET usersid=%%i
+	REG DELETE "HKEY_USERS\%usersid%\Software\Microsoft\Windows\CurrentVersion\Search\RecentApps" /f
+	REG ADD "HKEY_USERS\%usersid%\Software\Microsoft\Windows\CurrentVersion\Search\RecentApps"
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\bam\UserSettings\%usersid%" /va /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\UserSettings\%usersid%" /va /f
+REG DELETE "HKEY_USERS\%usersid%\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /va /f
+REG DELETE "HKEY_USERS\%usersid%\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2" /f
+REG ADD "HKEY_USERS\%usersid%\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2"
+DEL /f /q %APPDATA%\Microsoft\Windows\Recent\*.*
+DEL /f /q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*.*
+DEL /f /q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*.*
+ECHO Successfully
+DEL /f /q %systemroot%\Panther\*.*
+ECHO Successfully
+DEL /f /q %systemroot%\appcompat\Programs\*.txt
+DEL /f /q %systemroot%\appcompat\Programs\*.xml
+DEL /f /q %systemroot%\appcompat\Programs\Install\*.txt
+DEL /f /q %systemroot%\appcompat\Programs\Install\*.xml
+ECHO Successfully
+PAUSE
+EXIT
+:do_clear
+ECHO Clean %1
+wevtutil.exe cl %1
+GOTO :eof
+PAUSE
